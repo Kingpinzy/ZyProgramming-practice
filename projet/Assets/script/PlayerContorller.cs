97511e19a7472d69d1a01d372747af018687b9dc
Sucl <296744085@qq.com>
2021-11-08 18:35:18 +0800

    11.8,daima

--- projet/Assets/script/PlayerContorller.cs
+++ projet/Assets/script/PlayerContorller.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerContorller : MonoBehaviour
 {
     public string name = "zy";
-    public int health = 10;
-    public float attack = 2.2f;
+    public float health = 10;
+    public float atk = 2.2f;
+    public bool isbuff;//bool值初始值为false（假，未勾选）
+    
     // Start is called before the first frame update
     void Awake()//无论script是否启动，都开始执行
     {
@@ -14,13 +16,22 @@ public class PlayerContorller : MonoBehaviour
     }
     void Start()
     {
-        
-        Debug.Log(name+"'s health"+(health-attack));
+        /**health -= atk;
+        Debug.Log(name+"的攻击力"+health);
+        if (isbuff)
+        {
+            health += 5f;
+            Debug.Log("you are buff,now you health is"+health);
+        }**/
+
     }
 
     // Update is called once per frame每一帧都会刷新
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position(transform.)
+        }
     }
 }
