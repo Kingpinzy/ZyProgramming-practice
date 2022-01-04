ddd4a526ffed623720dbfea5b9416f7215e82f25
Sucl <296744085@qq.com>
2022-01-04 21:38:49 +0800

    function

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # ZyProgramming-practice
+
+### 2022.1.4
+
+#### 变量与变量的运算
+```c#
+	Debug.Log(name+"的攻击力"+health)
+	其中+号可作为连接符使用
+	a = a - b; 可以写作为 a -= b;
+	同理可得 += （拾取buff获得生命值等）
+	
+```
+
++ 循环语句
+```c#
+ if (health > atk)
+        {
+            health -= atk;
+            Debug.Log("你赢了"+health);
+            if (isbuff)
+            {
+                health += 5f;
+                Debug.Log("聪聪获得了buff，生命值增加5，当前生命为：" + health);
+            }
+        }
+        else
+            Debug.Log("你死了");
+
+    }
+void updata()
+{
+		if (Input.GetKeyDown(KeyCode.D))
+		        {
+		            transform.position = new Vector2(transform.position.x + 1,0);
+
+		        }
+}
+```
+循环语句，没什么好说的，其中穿插了一些变量的计算与bool值的运用。
+> 关于大小写的一些说明Transform与transform，其中Transform通常指代组件，transform通常指代组件当中的一些属性。
+---
+#### 函数Function
++ 函数分为有返回值&无返回值
+	+ 无返回值相当于一个代码的集合体，按顺序执行步骤
+	+ 
+
+
--- projet/Assets/script/PlayerContorller.cs
+++ projet/Assets/script/PlayerContorller.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerContorller : MonoBehaviour
 {
-    public string name = "zy";
-    public float health = 10;
-    public float atk = 2.2f;
-    public bool isbuff;//bool值初始值为false（假，未勾选）
     
+    public float health;
+    public float atk;
+    public bool isbuff;//bool值初始值为false（假，未勾选）
+
     // Start is called before the first frame update
     void Awake()//无论script是否启动，都开始执行
     {
-        
+
     }
     void Start()
     {
@@ -23,15 +23,45 @@ public class PlayerContorller : MonoBehaviour
             health += 5f;
             Debug.Log("you are buff,now you health is"+health);
         }**/
+        if (health > atk)
+        {
+            health -= atk;
+            Debug.Log("你赢了" + health);
+            if (isbuff)
+            {
+                health += 5f;
+                Debug.Log("聪聪获得了buff，生命值增加5，当前生命为：" + health);
+            }
+        }
+        else
+            Debug.Log("你死了");
 
+        Myinfo("zy", 26, true);
     }
+    
 
     // Update is called once per frame每一帧都会刷新
     void Update()
     {
+        Movement();
+    }
+    void Movement()
+    {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position(transform.)
+            transform.position = new Vector2(transform.position.x + 1, 0);
+
+        }
+    }
+    void Myinfo(string name,int age,bool isboy)
+    {
+        if (isboy)
+        {
+            Debug.Log("他的名字叫" + name+"他的年龄" + age+",他是个男孩");
+        }
+        else
+        {
+            Debug.Log("她的名字叫" + name + "她的年龄" + age + ",她是个女孩");
         }
     }
 }
